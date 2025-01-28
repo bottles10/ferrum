f0c9ee91f253f5429962b1a7c42518a2963e3c81
bottles10 <henryaovare@gmail.com>
2025-01-28 05:59:30 +0000

    Add headless chrome installation in render build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,29 @@
 # exit on error
 set -o errexit
 
+STORAGE_DIR=/opt/render/project/.render
+
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src # Make sure we return to where we were
+else
+  echo "...Using Chrome from cache"
+fi
+
+# be sure to add Chromes location to the PATH as part of your Start Command
+# export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+
+# add your own build commands...
+
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
@@ -10,4 +33,4 @@ bundle exec rails assets:clean
 # perform database migrations in the build command.
 # Uncomment the following line:
 
- bundle exec rails db:migrate
+bundle exec rails db:migrate
